0661898f67757853378caa49ce6b9f7b6ac2477e
ToniCoffee <96419343+ToniCoffee@users.noreply.github.com>
2021-12-21 19:41:19 +0100

    Delete scripts directory

--- scripts/services.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', async function (e) {
-  const response  = await includeHTMLAsync();
-
-  const beersData = JSON.parse(sessionStorage.getItem('beersData'));
-
-  setHeaderLogo(beersData); 
-
-  listeners(beersData); 
-}, false);
